008cc85609db6c67098a5b00c7696cf3ed99cde0
Marcelo Jose Da Silva <marcelo.silva@totvs.com.br>
2021-10-30 09:17:06 -0300

    primeiro commit card1

---
+++ main.js
@@ -0,0 +1,15 @@
+//import './style.css'
+// Aula 5 19:51
+import './src/styles/settings/colors.css'
+import './src/styles/generic/reset.css'
+import './src/styles/elements/base.css'
+
+import CardGame from "./src/components/CardGame";
+
+
+const $root = document.querySelector('#root');
+const $htmlCardGame  = CardGame();
+
+console.log($htmlCardGame);
+
+$root.insertAdjacentHTML('beforeend',$htmlCardGame);
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function CardGame() {
+    return `
+        <article class="card-game">
+            <img src="images/alura.png">
+        </article>    
+    `
+}
+
+export default CardGame;
