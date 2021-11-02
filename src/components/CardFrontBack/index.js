e266114d9e2e302a308bdd2c2daf6ed44cb6e2ae
Marcelo Jose Da Silva <marcelo.silva@totvs.com.br>
2021-11-02 17:47:51 -0300

    Add click event on CardFrontBack component

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,25 @@
 import CardGame from "../CardGame";
 import './style.css';
 
+
+
 function CardFrontBack() {
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event) => {
+        const $origin = event.target;
+        const $cardFrontBack =  $origin.closest('.card-front-back');
+
+        /*if($cardFrontBack.classList.contains('-active')){
+           $cardFrontBack.classList.remove('-active');
+        } else {
+            $cardFrontBack.classList.add('-active')        
+        }*/
+
+        $cardFrontBack.classList.toggle('-active');
+    }
 
     return /* Html*/ `
-        <article class="card-front-back">
+        <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardGame()} 
             </div> 
