da4d16a0f95cf452331eecfe23f5742be13a3d07
Marcelo Jose Da Silva <marcelo.silva@totvs.com.br>
2021-11-20 15:25:10 -0300

    Swap player when flip two cards

--- src/objetcs/BoardGame/index.js
+++ src/objetcs/BoardGame/index.js
@@ -3,13 +3,34 @@ import CardFrontBack from '../../components/CardFrontBack';
 import cards from "./data"
 
 function BoardGame(amountCards){     
+    const flipAndHideCards = ($cardsActive) => {
+        $cardsActive.forEach((card)=>card.classList.remove('-active'));
+    }
 
+    const swapPlayer = () => {
+        const $arrowDown = document.querySelector('.arrow-down');
+        const $currentPlayer = $arrowDown.getAttribute('data-currentplayer')
+
+        $arrowDown.setAttribute('data-currentplayer', $currentPlayer == 1 ? 2 : 1);
+    }
+
+    window.boardGame = {}
+    window.boardGame.handleClick = () => {
+       const $boardGama = document.querySelector('.board-game');       
+       const $cardsActive = $boardGama.querySelectorAll('.card-front-back.-active');
+       
+       if($cardsActive.length == 2){
+           setTimeout(() => {
+               flipAndHideCards($cardsActive)
+               swapPlayer()
+            }, 1000);
+        }       
+    }
     const htmlCardsList = cards.map((card) => CardFrontBack(card.icon), CardFrontBack(cards.altIcon));
     const $htmlCards = htmlCardsList.join('');
-
        
     return /*html*/ `
-        <section class="board-game">
+        <section class="board-game" onClick="boardGame.handleClick()">
             ${$htmlCards }
             
         </section>
